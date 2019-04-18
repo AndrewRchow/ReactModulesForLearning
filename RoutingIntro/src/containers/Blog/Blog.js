8c0ea9d4d61bd90abc7b520f6b7fc5401d699a66
Andrew Chow <chowlick@gmail.com>
2019-04-18 14:57:39 -0700

    Add link switching

--- RoutingIntro/src/containers/Blog/Blog.js
+++ RoutingIntro/src/containers/Blog/Blog.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-
+import {Route, Link} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPosts from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
         return (
-            <div class="Blog">
+            <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to='/new-post'>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={()=> <h1>Home</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
