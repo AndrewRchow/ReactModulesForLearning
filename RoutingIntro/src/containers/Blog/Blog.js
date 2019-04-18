c47077da129447b69065a064fa6e73971f49683f
Andrew Chow <chowlick@gmail.com>
2019-04-18 15:36:04 -0700

    pass route params

--- RoutingIntro/src/containers/Blog/Blog.js
+++ RoutingIntro/src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import {Route, NavLink} from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPosts from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render() {
@@ -25,6 +26,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={()=> <h1>Home</h1>} /> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPosts} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
