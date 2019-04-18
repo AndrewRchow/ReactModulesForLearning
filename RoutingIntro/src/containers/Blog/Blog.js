6169a68c16e67bb647472f7714b2f7ce28bc48bc
Andrew Chow <chowlick@gmail.com>
2019-04-18 16:44:31 -0700

    Switch to load single route

--- RoutingIntro/src/containers/Blog/Blog.js
+++ RoutingIntro/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, NavLink} from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -13,7 +13,7 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink to="/" exact 
+                            <li><NavLink to="/" exact
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
@@ -24,9 +24,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={()=> <h1>Home</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPosts} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPosts} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
