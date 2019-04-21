2de9ea51585da123535893d41065d26fcf8fe680
Andrew <chowlick@gmail.com>
2019-04-21 10:23:08 -0700

    404

--- RoutingIntro/src/containers/Blog/Blog.js
+++ RoutingIntro/src/containers/Blog/Blog.js
@@ -30,9 +30,9 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={()=> <h1>Home</h1>} /> */}
                 <Switch>
                     {this.state.auth ? <Route path="/new-post" component={NewPosts} /> : null}
-                    
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to='posts'/>
+                    <Route render={() =><h1>Not found</h1>} />
+                    {/* <Redirect from="/" to='posts'/> */}
                     {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
